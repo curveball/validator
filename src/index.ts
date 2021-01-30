36bd6e9d2331781b146322239031235d25957625
Evert Pot <me@evertpot.com>
2021-01-30 02:44:36 -0500

    Schema-api as middleware.
    This is a relatively large change, that does the following:
    
    1. Supply its functionality as a middleware, instead of a global
       function.
    2. Adds a new `ctx.request.validate` function to replace
       `schemaValidate`.
    3. It automatically creates a link from the 'home document to `/schemas`
       so you can easily find the schemas available in the system.
    4. It also lets you view schemas in hal-browser.
    
    I hoped I would get further tonight, but the next goal after this is to
    generate some (basic) HAL forms for schemas.

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,5 @@
-import { UnprocessableEntity } from '@curveball/http-errors';
-import Ajv2019 from 'ajv/dist/2019';
-import betterAjvErrors from 'better-ajv-errors';
-import fs from 'fs';
-import addFormats from 'ajv-formats';
+import './declarations';
+import '@curveball/links';
 
-const ajv = new Ajv2019();
-addFormats(ajv);
-
-export function addSchemasForDir(path: string) {
-  const schemas: string[] = [];
-  const files = fs.readdirSync(path);
-  for (const file of files) {
-    const fullPath = path + '/' + file;
-    const stat = fs.statSync(fullPath);
-    if (stat.isDirectory()) {
-      addSchemasForDir(fullPath);
-    } else {
-      schemas.push(fullPath);
-    }
-  }
-  for (const schema of schemas) {
-    const parsed = JSON.parse(fs.readFileSync(schema, 'utf-8'));
-    // eslint-disable-next-line no-console
-    console.log('Loading schema ' + parsed['$id']);
-    ajv.addSchema(parsed);
-  }
-}
-
-export function schemaValidate<T>(input: any, schema: string): T {
-  const result = ajv.validate(schema, input);
-  if (result) {
-    return input;
-  }
-
-  const error = ajv.errors;
-  const output = betterAjvErrors(schema, input, error, {format: 'js'});
-  if (!output) {
-    throw new Error('Unknown schema validation error');
-  }
-
-  // eslint-disable-next-line no-console
-  console.log(output);
-  throw new UnprocessableEntity(`JSON-Schema validation failed. ${output[0].error}`);
-}
+export { addSchemasForDir, schemaValidate } from './util';
+export { default as default } from './mw';
---
+++ src/util.ts
@@ -0,0 +1,56 @@
+import * as fs from 'fs';
+import { UnprocessableEntity } from '@curveball/http-errors';
+import Ajv2019 from 'ajv/dist/2019';
+import betterAjvErrors from 'better-ajv-errors';
+import addFormats from 'ajv-formats';
+import { SchemaInfo } from './types';
+
+export function findSchemas(path: string, localPrefix: string = ''): SchemaInfo[] {
+  const schemas: SchemaInfo[] = [];
+  const files = fs.readdirSync(path);
+  for (const file of files) {
+    const fullPath = path + '/' + file;
+    const stat = fs.statSync(fullPath);
+    if (stat.isDirectory()) {
+      schemas.push(...findSchemas(fullPath, localPrefix + file + '/'));
+    } else {
+      const parsed = JSON.parse(fs.readFileSync(fullPath,'utf-8'));
+      schemas.push({
+        id: parsed.$id,
+        description: parsed.description,
+        path: localPrefix + file,
+        schema: parsed,
+      });
+    }
+  }
+  return schemas;
+}
+
+const ajv = new Ajv2019();
+addFormats(ajv);
+
+export function addSchemasForDir(path: string): void {
+  const schemas = findSchemas(path);
+  for (const schema of schemas) {
+    // eslint-disable-next-line no-console
+    console.log('Loading schema ' + schema.id);
+    ajv.addSchema(schema.schema);
+  }
+}
+
+export function schemaValidate<T>(input: any, schema: string): T {
+  const result = ajv.validate(schema, input);
+  if (result) {
+    return input; 
+  }
+
+  const error = ajv.errors;
+  const output = betterAjvErrors(schema, input, error, {format: 'js'});
+  if (!output) {
+    throw new Error('Unknown schema validation error');
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(output);
+  throw new UnprocessableEntity(`JSON-Schema validation failed. ${output[0].error}`);
+}
