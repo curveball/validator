26e2e2955acdfdc120cdcf9e5e84963e38a32d89
Evert Pot <me@evertpot.com>
2021-02-01 22:37:43 -0500

    Fix assertion

--- src/declarations.ts
+++ src/declarations.ts
@@ -1,10 +1,9 @@
-import { Context } from '@curveball/core';
 import '@curveball/core';
 
 declare module '@curveball/core' {
 
   interface Request {
-    validate: <T>(schemaId: string) => asserts this is Context<T>;
+    validate: <T>(schemaId: string) => asserts this is Request<T>;
   }
 
 }
